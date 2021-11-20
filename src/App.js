e71ff9c106f0ca563b6d7d103305d7f843278e8c
Connor Peoples <connor@connorpeoples.com>
2021-11-19 23:33:10 -0600

    Updated metta information. Added Container to app.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import UselessHeader from './Framing/Header.js';
 import UselessFooter from './Framing/Footer.js';
 import WebSites from './Interface/Websites.js';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
       <UselessHeader />
+      <Container>
+        <WebSites />
+      </Container>
       <UselessFooter />
-      <WebSites />
     </>
   );
 }
